1f5c71507d613f0ce257b1c429d3ceedcba17426
Parul Bhardwaj <bhardwaj.parul88@gmail.com>
2023-06-09 21:06:49 -0400

    Users can join chat and send messages

--- public/index.js
+++ public/index.js
@@ -1,15 +1,33 @@
 $(document).ready( () => {
   //Connect to the socket.io server
   const socket = io.connect();
+
+  //Keep track of the current user
+  let currentUser;
   $('#create-user-btn').click((e)=>{
     e.preventDefault();
-    let username = $('#username-input').val();
-    if(username.length > 0){
+    if($('#username-input').val().length > 0){
       //Emit to the server the new user
-      socket.emit('new user', username);
+      socket.emit('new user', $('#username-input').val());
+      // Save the current user when created
+      currentUser = $('#username-input').val();
       $('.username-form').remove();
       // Have the main page visible
       $('.main-container').css('display', 'flex');
+    }
+  });
+  $('#send-chat-btn').click((e) => {
+    e.preventDefault();
+    // Get the message text value
+    let message = $('#chat-input').val();
+    // Make sure it's not empty
+    if(message.length > 0){
+      // Emit the message with the current user to the server
+      socket.emit('new message', {
+        sender : currentUser,
+        message : message,
+      });
+      $('#chat-input').val("");
     }
   });
   //socket listeners
@@ -17,5 +35,15 @@ $(document).ready( () => {
     console.log(`✋ ${username} has joined the chat! ✋`);
     // Add the new user to the online users div
     $('.users-online').append(`<div class="user-online">${username}</div>`);
+  });
+
+  //Output the new message
+  socket.on('new message', (data) => {
+    $('.message-container').append(`
+      <div class="message">
+        <p class="message-user">${data.sender}: </p>
+        <p class="message-text">${data.message}</p>
+      </div>
+    `);
   });
 })
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,10 +1,16 @@
 module.exports = (io, socket) => {
     // Listen for "new user" socket emits
     socket.on('new user', (username) => {
-    console.log(`${username} has joined the chat! ✋`);
+    console.log(`✋${username} has joined the chat! ✋`);
     //Send the username to all clients currently connected
     // io.emit sends data to all clients on the connection.
     // socket.emit sends data to the client that sent the original data to the server.
     io.emit("new user", username);
+    });
+    //Listen for new messages
+    socket.on('new message', (data) => {
+        // Send that data back to ALL clients
+        console.log(`🎤 ${data.sender}: ${data.message} 🎤`)
+        io.emit('new message', data);
     })
 }
