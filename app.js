e8a2c46c39ee5a4eed5ebfefae8385817a519bcf
Parul Bhardwaj <bhardwaj.parul88@gmail.com>
2023-06-09 21:40:30 -0400

    Users can create new channels and join

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ const server = require('http').Server(app);
 
 //Socket.io
 const io = require('socket.io')(server);
-io.on("connection", (socket) => {
   //We'll store our online users here
   let onlineUsers = {};
+  //Save the channels in this object.
+  let channels = {"General" : []};
+
+  io.on("connection", (socket) => {
   // This file will be read on new socket connections
-  // Make sure to send the users to our chat file
-  require('./sockets/chat.js')(io, socket, onlineUsers);
-})
+  // Make sure to send the users and the channels to our chat file
+  require('./sockets/chat.js')(io, socket, onlineUsers, channels);
+});
 
 //Express View Engine for Handlebars
 const exphbs  = require('express-handlebars');
@@ -25,4 +28,4 @@ app.get('/', (req, res) => {
 
 server.listen('3000', () => {
   console.log('Server listening on Port 3000');
-});
+})
