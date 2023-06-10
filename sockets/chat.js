56e268eae1f7b956464463e8b7ad5bc250649c18
Parul Bhardwaj <bhardwaj.parul88@gmail.com>
2023-06-09 20:29:47 -0400

    Users can log in

---
+++ sockets/chat.js
@@ -0,0 +1,9 @@
+module.exports = (io, socket) => {
+    // Listen for "new user" socket emits
+    socket.on('new user', (username) => {
+    console.log(`${username} has joined the chat! ✋`);
+    //Send the username to all clients currently connected
+    // io.emit sends data to all clients on the connection.
+    io.emit("new user", username);
+    })
+}
